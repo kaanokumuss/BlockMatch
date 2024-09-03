0217a1dac322d26dae8cdefad6e4eeea7e350579
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-09-03 11:36:59 +0300

    CreateCells Methods Created

--- Assets/Scripts/Game/BoardBase/Board.cs
+++ Assets/Scripts/Game/BoardBase/Board.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.Experimental.Playables;
+using UnityEngine.Rendering.UI;
 using Zenject;
 
 public class Board : MonoBehaviour
@@ -9,4 +11,28 @@ public class Board : MonoBehaviour
     public int Rows { get; private set; }
     public int Columns { get; private set; }
     public Cell[,] Cells { get; private set;} // Celleri tutacagimiz bir matrix
+
+    public void Prepare(int row , int columns)
+    {
+        Rows = row;
+        Columns = columns;
+
+        Cells = new Cell[Rows, Columns];
+
+        CreateCells();
+        
+    }
+
+    private void CreateCells()
+    {
+        for (int x = 0; x < Rows; x++)
+        {
+            for (int y = 0; y < Columns; y++)
+            {
+                var cell = _cellFactory.Create(); /// Instantiate etti 
+                cell.transform.SetParent(cellParent); // Instantiate edeceği parent  
+                Cells[x, y] = cell; // cellimizin matrix değerlerini kaydetti 
+            }
+        }
+    }
 }

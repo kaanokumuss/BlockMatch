5e23b1b529b216d75a52e6baa0d18791b22e8bc3
kaanokumuss53 <kaanokumuss53@gmail.com>
2024-09-04 09:46:30 +0300

    Update Board.cs

--- Assets/Scripts/Game/BoardBase/Board.cs
+++ Assets/Scripts/Game/BoardBase/Board.cs
@@ -29,9 +29,9 @@ public class Board : MonoBehaviour
     {
         Rows = row; 
         Columns = columns;
-        // Aldığımız Değerleri olusturduğumuz Rows ve Columns a eşitliyoruz.
-        Cells = new Cell[Rows, Columns]; //Cell'imiz boş bir matrixken biz ona değerlerimizi atıyoruz. 
-        CreateCells(); //CreateCell methodunu çağırıyoruz.
+        
+        Cells = new Cell[Rows, Columns]; 
+        CreateCells(); 
         PrepareCells();
     }
 
@@ -40,6 +40,56 @@ public class Board : MonoBehaviour
         var cell = signal.Touchable.gameObject.GetComponent<Cell>(); // signal ile tikladigimiz gameobjecti aliyoruz. 
     }
 
+    private Cell GetNeighbourWithDirection(Cell cell, Direction direction)
+    {
+        var x = cell.X;
+        var y = cell.Y;
+
+        switch (direction)
+        {
+            case Direction.None:
+                break;
+            case Direction.Up:
+                y += 1;
+                break;
+            case Direction.UpRight:
+                y += 1;
+                x += 1;
+
+                break;
+            case Direction.Right:
+                x += 1;
+                break;
+            case Direction.DownRight:
+                y -= 1;
+                x += 1;
+                break;
+            case Direction.Down:
+                y -= 1;
+
+                break;
+            case Direction.DownLeft:
+                y -= 1;
+                x -= 1;
+                break;
+            case Direction.Left:
+                x -= 1;
+                break;
+            case Direction.UpLeft:
+                y += 1;
+                x -= 1;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+
+        if (x< 0 || x >= Rows || y<0 || y>=Columns)
+        {
+            return null;
+        }
+
+        return Cells[x, y];
+    }
 
     private void CreateCells()
     {
